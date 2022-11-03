66a438c2958b332eee722bb97a95ef5bb9fe553e
James FLaws <Flaws77@aol.com>
2022-11-03 09:06:16 -0700

    schema for postgresql.

---
+++ src/Schema.sql
@@ -0,0 +1,39 @@
+DROP DATABASE IF EXISTS productquestions;
+
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS answers;
+DROP TABLE IF EXISTS answerphotos;
+
+CREATE DATABASE productquestions;
+
+CREATE TABLE questions (
+  questions_id: INT NOT NULL,
+  product_id: INT NOT NUll,
+  question_body: TEXT NOT NULL,
+  asker_name: VARCHAR(255) NOT NULL,
+  asker_email: VARCHAR(255) NOT NULL,
+  date_written: DATE DEFAULT CURRENT_TIMESTAMP,
+  helpfulness: INT DEFAULT 0,
+  answers: INT[],
+  PRIMARY KEY ('questions_id')
+)
+
+CREATE TABLE answers (
+  questions_id: INT NOT NULL,
+  answer_id: INT NOT NUll,
+  answer_body: TEXT NOT NULL,
+  answerer_name: VARCHAR(255) NOT NULL,
+  answerer_email: VARCHAR(255) NOT NULL,
+  date_written: DATE DEFAULT CURRENT_TIMESTAMP,
+  helpfulness: INT DEFAULT 0,
+  PRIMARY KEY('answer_id'),
+  FOREIGN KEY ('questions_id') REFERENCES questions('questions_id')
+)
+
+CREATE TABLE photos (
+  id: INT NOT NULL,
+  answer_id: INT NOT NULL,
+  url: text,
+  PRIMARY KEY ('id'),
+  FOREIGN KEY ('anser_id') REFERENCES answers('answer_id')
+)
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,26 +1,30 @@
 const mongoose = require('mongoose');
 
-const answerSchema = new Schema {
+const answerSchema = new mongoose.Schema {
   answer_id: {type: Number, Unique: true},
   answer_body: String,
-  Answerer: String,
-  date {type: Date, Default: date.now},
+  answerer_name: String,
+  answerer_email: String
+  date_written: {type: Date, Default: date.now},
   helpfulness: Number,
-  reported {type: Boolean, default: false},
+  //reported: {type: Boolean, default: false},
   Photos:  [String]
 }
-const questionSchema = new Schema {
+const questionSchema = new mongoose.Schema {
   question_id: {type: Number, Unique: true},
-  Question_body: String,
-  Question_asker: String,
-  date: {type: Date, default: date.now}
+  question_body: String,
+  asker_name: String,
+  asker_email: String,
+  date_written: {type: Date, default: date.now}
   helpfulness: Number,
-  reported {type: Boolean, default: false}
+  //reported: {type: Boolean, default: false}
   answers: [type: answerSchema],
 }
-const productschema = new schema {
+const productschema = new mongoose.schema {
   product_id: {type: Number, Unique: true},
   questions: [{type: questionSchema}]
 }
 
 const ProductQuestions = mongoose.model('ProductQueastions', productschema);
+
+module.exports = ProductQueastions;
